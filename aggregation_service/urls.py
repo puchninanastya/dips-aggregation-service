9db12071c48b5269c8f56450aa5ed7a8bcb07483
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-18 18:14:43 +0300

    User, Order, Billing services added.

--- aggregation_service/urls.py
+++ aggregation_service/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+    url(r'^users/$', views.UserList.as_view(), name='users-list'),
+    url(r'^users/(?P<id>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
     url(r'^courses/$', views.CourseList.as_view(), name='courses-list'),
     url(r'^courses/(?P<id>[0-9]+)/$', views.CourseDetail.as_view(), name='course-detail'),
+    url(r'^orders/$', views.OrderList.as_view(), name='orders-list'),
+    url(r'^orders/(?P<id>[0-9]+)/$', views.OrderDetail.as_view(), name='order-detail'),
+    url(r'^payments/$', views.PaymentList.as_view(), name='payments-list'),
+    url(r'^payments/(?P<id>[0-9]+)/$', views.PaymentDetail.as_view(), name='payment-detail'),
 ]
--- aggregation_service/views.py
+++ aggregation_service/views.py
@@ -57,3 +57,157 @@ class CourseDetail(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class UserList(APIView):
+    url = 'http://127.0.0.1:8001/users/'
+
+    def get(self, request):
+        try:
+            r = requests.get(self.url)
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            r = requests.post(self.url, data = request.data)
+            if r.status_code == requests.codes.created:
+                return Response(r.json(), status=status.HTTP_201_CREATED)
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(APIView):
+    url = 'http://127.0.0.1:8001/users/'
+
+    def get(self, request, id):
+        try:
+            r = requests.get(self.url+id+'/')
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, id):
+        try:
+            r = requests.put(self.url+id+'/', data = request.data)
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            r = requests.delete(self.url+id+'/')
+            if r.status_code == 204:
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class OrderList(APIView):
+    url = 'http://127.0.0.1:8003/orders/'
+
+    def get(self, request):
+        try:
+            r = requests.get(self.url)
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            r = requests.post(self.url, data = request.data)
+            if r.status_code == requests.codes.created:
+                return Response(r.json(), status=status.HTTP_201_CREATED)
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class OrderDetail(APIView):
+    url = 'http://127.0.0.1:8003/orders/'
+
+    def get(self, request, id):
+        try:
+            r = requests.get(self.url+id+'/')
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, id):
+        try:
+            r = requests.put(self.url+id+'/', data = request.data)
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            r = requests.delete(self.url+id+'/')
+            if r.status_code == 204:
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class PaymentList(APIView):
+    url = 'http://127.0.0.1:8004/payments/'
+
+    def get(self, request):
+        try:
+            r = requests.get(self.url)
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            r = requests.post(self.url, data = request.data)
+            if r.status_code == requests.codes.created:
+                return Response(r.json(), status=status.HTTP_201_CREATED)
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class PaymentDetail(APIView):
+    url = 'http://127.0.0.1:8004/payments/'
+
+    def get(self, request, id):
+        try:
+            r = requests.get(self.url+id+'/')
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, id):
+        try:
+            r = requests.put(self.url+id+'/', data = request.data)
+            if r.status_code == requests.codes.ok:
+                return Response(r.json())
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            r = requests.delete(self.url+id+'/')
+            if r.status_code == 204:
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
